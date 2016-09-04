23b7ce990fde354f5ea25a1a28d6f2190cbbe667
Sultanidze <sultanidze@ukr.net>
2016-09-04 22:56:15 +0300

    5.7 Showing Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,14 @@
 class ArticlesController < ApplicationController
-  def new
+  def show
+    @article = Article.find(params[:id])
   end
-  
+
   def create
     @article = Article.new(params.require(:article).permit(:title, :text))
- 
     @article.save
     redirect_to @article
+  end
+  
+  def new
   end
 end
