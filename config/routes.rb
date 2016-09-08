ebf6f5f613a62b889abe37ab18860702e79a3a87
Sultanidze <sultanidze@ukr.net>
2016-09-08 16:50:32 +0300

    6.3 Adding a Route for Comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :articles
   root 'welcome#index'
+
+  resources :articles do
+    resources :comments
+  end
+
 end
+
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
